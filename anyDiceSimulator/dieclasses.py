261fe171b1e0a78047a6eb0a3c592a6259e3463a
Neha-mittal-123 <neha.vineeta@gmail.com>
2025-08-17 15:48:00 -0700

    Commit changes

--- anyDiceSimulator/dieclasses.py
+++ anyDiceSimulator/dieclasses.py
@@ -14,6 +14,7 @@ class Die:
         print("Biased: False")
         print("Side weights: None")
         print("==========================================")
+        print("Neha is the best")
 
     def rollDie(self, n):
         print(f"Number of rolls: {n}")
